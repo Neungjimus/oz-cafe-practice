16150b0bbbdc0289e5a828f52a0ca69a5ba24bf1
Neungjimus <lingchihhao@gmail.com>
2025-07-15 21:36:03 +0900

    cafe-pracitce 과제 완료

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,20 @@
+import { useCart } from "../context/cartContext";
+import { useMenu } from "../context/menuContext";
 import data from "../assets/data";
 
-function Cart({ menu, cart, setCart }) {
+function Cart() {
+  const { cart, setCart } = useCart();
+  const { menu } = useMenu();
+
   if (!menu)
     return (
       <div style={{ textAlign: "center", margin: "80px" }}>
         메뉴 정보가 없어요!
       </div>
     );
+
   const allMenus = [...menu.커피, ...menu.논커피];
+
   return (
     <>
       <h2>장바구니</h2>
@@ -19,8 +26,7 @@ function Cart({ menu, cart, setCart }) {
               item={allMenus.find((menu) => menu.id === el.id)}
               options={el.options}
               quantity={el.quantity}
-              cart={cart}
-              setCart={setCart}
+              id={el.id}
             />
           ))
         ) : (
@@ -31,16 +37,18 @@ function Cart({ menu, cart, setCart }) {
   );
 }
 
-function CartItem({ item, options, quantity, cart, setCart }) {
+function CartItem({ item, options, quantity, id }) {
+  const { cart, setCart } = useCart();
+
   return (
     <li className="cart-item">
       <div className="cart-item-info">
-        <img height={100} src={item.img} />
+        <img height={100} src={item.img} alt={item.name} />
         <div>{item.name}</div>
       </div>
       <div className="cart-item-option">
         {Object.keys(options).map((el) => (
-          <div key={el.id}>
+          <div key={el}>
             {el} : {data.options[el][options[el]]}
           </div>
         ))}
@@ -49,7 +57,7 @@ function CartItem({ item, options, quantity, cart, setCart }) {
       <button
         className="cart-item-delete"
         onClick={() => {
-          setCart(cart.filter((el) => item.id !== el.id));
+          setCart(cart.filter((el) => el.id !== id));
         }}
       >
         삭제
@@ -57,4 +65,5 @@ function CartItem({ item, options, quantity, cart, setCart }) {
     </li>
   );
 }
+
 export default Cart;
---
+++ src/context/menuContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useContext } from "react";
+import menuData from "../assets/data/Menu"; 
+
+const MenuContext = createContext();
+
+export function MenuProvider({ children }) {
+  const [menu, setMenu] = useState(menuData);
+
+  return (
+    <MenuContext.Provider value={{ menu }}>
+      {children}
+    </MenuContext.Provider>
+  );
+}
+
+export function useMenu() {
+  return useContext(MenuContext);
+}
